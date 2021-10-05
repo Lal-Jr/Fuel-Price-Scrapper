7af889ac95927c88b05096e8db76afa6a9f81a97
Harish Lal <69763020+Lal-Jr@users.noreply.github.com>
2021-10-05 12:00:34 +0530

    final commit

--- petrol_scraper.py
+++ petrol_scraper.py
@@ -2,8 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import matplotlib.pyplot as plt
-import csv
-import numpy as np
 
 URL = "https://www.newsrain.in/petrol-diesel-prices"
 page = requests.get(URL)
@@ -43,37 +41,13 @@ for article in articles:
             dieselPrice.append(productPrice)
 
 columnName = ["Place", "Petrol_Price", "Diesel_Price"]
-
-df = pd.DataFrame([place, petrolPrice,
-                  dieselPrice], index=columnName)
+df = pd.DataFrame([place, petrolPrice, dieselPrice], index=columnName)
 df = df.T
 df.to_csv('fuelprice.csv')
 
-# x = []
-# y = []
-
-# with open('fuelprice.csv', 'r') as file:
-#     plots = csv.reader(file, delimiter=',')
-
-#     for row in plots:
-#         x.append(row[1])
-#         y.append(row[2])
-
-# plt.bar(x, y, color='g', label="Fuel")
-# plt.xlabel('Place')
-# plt.ylabel('Price')
-# plt.title('Fuel Price Comparison')
-# plt.legend()
-# plt.show()
-
-index = np.arange(36)
-fig = plt.figure()
-ax = fig.add_axes([0, 0, 1, 1])
-ax.bar(index, petrolPrice, color='r')
-ax.bar(index, dieselPrice, color='b')
-ax.set_ylabel('Price')
-ax.set_title('Fuel Price Comparison')
-ax.set_xticks(index)
-ax.set_yticks(np.arange(80, 111, 2))
-ax.legend(labels=['Petrol', 'Diesel'])
+graph = pd.read_csv("./fuelprice.csv")
+plt.barh(graph['Place'], graph['Petrol_Price'], color = 'b')
+plt.barh(graph['Place'], graph['Diesel_Price'], color = 'g')
+plt.xlabel("Fuel Price")
+plt.ylabel("Place")
 plt.show()
